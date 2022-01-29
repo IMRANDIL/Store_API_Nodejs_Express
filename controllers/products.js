fa221397146fdf9064d34ee30d384fd0c6b7011e
IMRANDIL <aliimranadil2@gmail.com>
2022-01-29 18:31:23 +0530

    yh

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ page: '2' });
+    const products = await Product.find({ featured: true });
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -10,8 +10,19 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
 
+    const { featured, company } = req.query;
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
 
-    const products = await Product.find(req.query)
+    if (company) {
+        queryObject.company = company;
+    }
+
+
+    console.log(queryObject);
+    const products = await Product.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
 }
 
